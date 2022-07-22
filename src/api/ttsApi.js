1c432a58f7fa9932944cbc4d452d7d3ecf2a1fae
janechoi8 <janechoi2001@gmail.com>
2022-07-22 22:05:23 +0700

    setup api

---
+++ src/api/ttsApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const textToSpeechApi = async (data, voice) =>{
+    const response = await axios.post('https://api.fpt.ai/hmi/tts/v5', data, {
+        headers:{
+            'api-key': 'VoUtARimHhggu9TlH25HPki59gK1Hxiy',
+            'voice': voice,
+        }
+    });
+    return response;
+}
+
+export default textToSpeechApi;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from "react";
-import Select from "react-select";
-import icConvert from "../../assets/ic_convert.png";
+import React, { useState } from 'react';
+import Select from 'react-select';
+import textToSpeechApi from '../../api/ttsApi';
+import icConvert from '../../assets/ic_convert.png';
 
-import "./home.sass";
+import './home.sass';
 
 const options = [
-  { value: "english", label: "English" },
-  { value: "vietnamese", label: "Vietnamese" },
-  { value: "spanish", label: "Spanish" },
-  { value: "chinese", label: "Chinese" },
+  { value: 'english', label: 'English' },
+  { value: 'vietnamese', label: 'Vietnamese' },
+  { value: 'spanish', label: 'Spanish' },
+  { value: 'chinese', label: 'Chinese' },
 ];
-function Home() {
-  const [text, setText] = useState("");
 
+function Home() {
+  const [text, setText] = useState('');
+
+  const convert = async () => {
+    const response = await textToSpeechApi(text, 'banmai');
+    console.log(response);
+
+    const audio = new Audio(response.data.async);
+    audio.play();
+  };
   return (
     <div className="container">
       <div className="field">
@@ -22,6 +31,8 @@ function Home() {
           className="field__textarea"
           name="text"
           placeholder="Enter your text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
       </div>
 
@@ -30,14 +41,21 @@ function Home() {
 
         <Select
           isSearchable={false}
-          defaultValue={{ label: "English", value: "english" }}
+          defaultValue={{
+            label: 'English',
+            value: 'english',
+          }}
           className="field__select"
           options={options}
         />
       </div>
 
-      <button className="submit">
-        <img className="submit__icon" src={icConvert} alt="Covert icon" />
+      <button className="submit" onClick={convert}>
+        <img
+          className="submit__icon"
+          src={icConvert}
+          alt="Covert icon"
+        />
         <p className="submit__text">Convert</p>
       </button>
     </div>
