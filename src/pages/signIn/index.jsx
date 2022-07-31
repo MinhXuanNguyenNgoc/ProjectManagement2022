6f3ad34c8573e7e26e929328e6cdda4bb91a5367
janechoi8 <janechoi2001@gmail.com>
2022-07-31 13:31:58 +0700

    update login

--- src/pages/signIn/index.jsx
+++ src/pages/signIn/index.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import "./signin.sass";
 import { Link } from "react-router-dom";
 
+const accounts = [
+  {username: "minhxuan123", password: "123456789"},
+  {username: "nguyenphuong", password: "19090109"},
+  {username: "maiphuong", password: "100119089"},
+]
+
 const SignIn = () => {
+  const[user, setUsername] = useState('');
+  const [pw, setPw] = useState('');
+
+  const register = () =>{
+    for (const element of accounts) {
+      if (user === element.username) {
+        if (pw === element.password) {
+          alert('Login successful!!!')
+          break;
+        }
+        else {
+          alert('Incorrect username or password')
+        }
+        break;
+      }
+      else{
+        alert('Incorrect username or password')
+        break;
+      }
+    }
+  }
+
   return (
     <div className="signin">
       <div className="signin__container">
         <h1 className="signin__title">Log in</h1>
-        <input type="text" placeholder="Username" className="signin__input" />
-        <input type="password" placeholder="Password" className="signin__input" />
+        <input type="text" placeholder="Username" className="signin__input" value={user} onChange={(e) => setUsername(e.target.value)}/>
+        <input type="password" placeholder="Password" className="signin__input" value={pw} onChange={(e) => setPw(e.target.value)} />
         <div className="signin__row">
           <Link className="signin__forgot" to="/authentication/forgot">
             Forgot password?
@@ -17,7 +45,7 @@ const SignIn = () => {
             Sign up
           </Link>
         </div>
-        <button className="signin__button">Login</button>
+        <button className="signin__button" onClick={register}>Login</button>
       </div>
     </div>
   );
